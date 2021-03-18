2d612f2e316ded2131f145ee278aa0062afa8de1
Chase A Smith <74937395+smit9804@users.noreply.github.com>
2021-03-18 18:28:19 -0500

    Add files via upload

---
+++ FizzBuzz/FizzBuzz.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+
+public class FizzBuzz {
+    public String fizzBuzz(int number) {
+        if (number % 5 == 0 && number % 3 == 0) {
+            return "FizzBuzz";
+        }
+        else if (number % 3 == 0) {
+            return "Fizz";
+        }
+        else if (number % 5 == 0) {
+            return "Buzz";
+        }
+        else {
+            return Integer.toString(number);
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,7 @@
+public class FizzBuzzTest {
+    public static void main (String[] args){
+        // buzz = FizzBuzz();
+        FizzBuzz buzz = new FizzBuzz();
+        System.out.println(buzz.fizzBuzz(8));
+    }
+}
